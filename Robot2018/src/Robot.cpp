93e099b13ac6d3131137a692605b32097e414923
Tigertronics <team2053tigertronics@gmail.com>
2018-03-14 18:29:18 -0400

    hopefully made substrings right

--- Robot2018/src/Robot.cpp
+++ Robot2018/src/Robot.cpp
@@ -160,9 +160,9 @@ void Robot::AutonomousInit() {
 			}
 
 			std::cout << "toPath: " << toPath << std::endl;
-			std::cout << "switchPath: " << toPath.substr(0,3) << std::endl;
-			std::cout << "scalePath: " << toPath.substr(2,2) << std::endl;
-			LoadChosenPath(toPath.substr(0,3), toPath.substr(2,2));
+			std::cout << "switchPath: " << toPath.substr(0,2) << std::endl;
+			std::cout << "scalePath: " << toPath.substr(1,2) << std::endl;
+			LoadChosenPath(toPath.substr(0,2), toPath.substr(1,2));
 
 			cmdSwitch = new FollowPath(trajToSwitch, lengthOfSwitchTraj, 0);
 			std::cout << "Switch Pathfinder Trajectory Points: " << lengthOfSwitchTraj << std::endl;
